66d3a954675f514c81449e723cc2670febf4ca67
unknown <amoniyanemmanuel@gmail.com>
2024-10-30 21:35:44 +0100

    (week-3)

---
+++ week-3/practice_1/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let school_string = "School of Science and Technology";  // string type
+    let rating_float = 5.0;      // float type
+    let is_growing_boolean = true;      // boolean type
+    let department_ineger = 3;     // integer type
+
+    println!("School Name is: {}",school_string);
+    println!("School rating on 5 1s: {}",rating_float);
+    println!("School is growing: {}",is_growing_boolean);
+    println!("Number of Departments : {}",department_ineger);
+}
