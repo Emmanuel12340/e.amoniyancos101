7a07359fe2e2e8ea0a26624e867e4b2e24dd1bd0
unknown <amoniyanemmanuel@gmail.com>
2024-11-05 13:41:50 +0100

    (week-2)

---
+++ week-2/Practice-2.rs
@@ -0,0 +1,6 @@
+fn main{
+    println!();
+    println!("hello");
+    println!("format {} agruments", "some")
+    println!("My name is {}. I am a member of stream 2", "Amoniyan Emmanuel Bolaji")
+}
